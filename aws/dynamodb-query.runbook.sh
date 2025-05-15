d3c194821804b75856df4948b1245496e40db629
bharadwaja-liveramp <bharadwaja.vissavajhula@liveramp.com>
2025-05-15 18:50:29 +0530

    Update dynamodb-query.runbook.sh with PII tables
    Added the PII tables in use in dynamo.

--- aws/dynamodb-query.runbook.sh
+++ aws/dynamodb-query.runbook.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -eo pipefail
 
-TABLE_NAME='{{ .tableName | type "select" | description "the name of the table" | options "Movies" "ats-capi-config-ats" }}'
+TABLE_NAME='{{ .tableName | type "select" | description "the name of the table" | options "ats-placement-versions-dev" "ats-direct-v2-configs-dev" "user-details-dev" "launchpadVersions-dev" "multiAccounts-dev" }}'
 KEY_CONDITION_EXPR='{{ .keyExpression | description "The condition that specifies the key values for items to beretrieved by the Query action" | required "keyExpression is required" }}'
 EXPR_ATTRIBUTE_NAMES='{{ .exprAttributeNames | description "One or more substitution tokens for attribute names in an expression" | required "exprAttributeNames is required" }}'
 EXPR_ATTRIBUTE_VALUES='{{ .exprAttributeValues | description " One or more values that can be substituted in an expression" | required "exprAttributeValues is required" }}'
